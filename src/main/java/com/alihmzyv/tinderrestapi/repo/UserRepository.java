2f64b5be7e6318a940c1d0f2dda36a57d02215a8
alihmzyv <alihmzyv@gmail.com>
2022-11-14 21:20:30 +0400

    added findByUsernameAndPassword query method

--- src/main/java/com/alihmzyv/tinderrestapi/repo/UserRepository.java
+++ src/main/java/com/alihmzyv/tinderrestapi/repo/UserRepository.java
@@ -3,12 +3,16 @@ package com.alihmzyv.tinderrestapi.repo;
 import com.alihmzyv.tinderrestapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+import org.springframework.data.rest.core.annotation.RestResource;
 import java.util.Optional;
 
 
-//just to make it clear. actually no need to explicitly set path to "messages", since it is that by default.
+//just to make it clear. actually no need to explicitly set path to "users", since it is that by default.
 @RepositoryRestResource(path = "users")
+@RestResource
 public interface UserRepository extends JpaRepository<User, Long> {
+    @RestResource(path = "findByEmailAndPassword")
     Optional<User> findByEmailAddressAndPassword(String emailAddress, String password);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
