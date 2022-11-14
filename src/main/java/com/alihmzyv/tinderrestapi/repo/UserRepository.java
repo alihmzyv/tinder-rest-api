2657dea514138466bade01373d1028239682efa4
alihmzyv <alihmzyv@gmail.com>
2022-11-14 18:30:35 +0400

    added findByEmailAddressAndPassword

--- src/main/java/com/alihmzyv/tinderrestapi/repo/UserRepository.java
+++ src/main/java/com/alihmzyv/tinderrestapi/repo/UserRepository.java
@@ -4,8 +4,11 @@ import com.alihmzyv.tinderrestapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 
 //just to make it clear. actually no need to explicitly set path to "messages", since it is that by default.
 @RepositoryRestResource(path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailAddressAndPassword(String emailAddress, String password);
 }
