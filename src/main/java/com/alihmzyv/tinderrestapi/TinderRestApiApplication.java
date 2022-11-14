509b5df391ea5c1382de572c85e1c38c12b91d5a
alihmzyv <alihmzyv@gmail.com>
2022-11-14 13:29:36 +0400

    formatted

--- src/main/java/com/alihmzyv/tinderrestapi/TinderRestApiApplication.java
+++ src/main/java/com/alihmzyv/tinderrestapi/TinderRestApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TinderRestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TinderRestApiApplication.class, args);
 	}
-
 }
