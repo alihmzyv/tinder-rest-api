3e66e37f5baa1cbbcaa5152148bb17b8c4bf285f
alihmzyv <alihmzyv@gmail.com>
2022-11-14 13:28:52 +0400

    explicitly defined the path

--- src/main/java/com/alihmzyv/tinderrestapi/repo/UserRepository.java
+++ src/main/java/com/alihmzyv/tinderrestapi/repo/UserRepository.java
@@ -2,6 +2,10 @@ package com.alihmzyv.tinderrestapi.repo;
 
 import com.alihmzyv.tinderrestapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+//just to make it clear. actually no need to explicitly set path to "messages", since it is that by default.
+@RepositoryRestResource(path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
 }
