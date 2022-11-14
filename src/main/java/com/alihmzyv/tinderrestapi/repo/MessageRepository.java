6783f1a118261a3d7110a0f00f1cf18f7724b3ac
alihmzyv <alihmzyv@gmail.com>
2022-11-14 21:19:44 +0400

    removed the url comment

--- src/main/java/com/alihmzyv/tinderrestapi/repo/MessageRepository.java
+++ src/main/java/com/alihmzyv/tinderrestapi/repo/MessageRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 //just to make it clear. actually no need to explicitly set path to "messages", since it is that by default.
 @RepositoryRestResource(path = "messages")
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    // http://localhost:8080/tinder-api/messages/search/findAllBetween{?user1Id,user2Id}
     @Query("from Message where (fromId = ?1 and toId = ?2) or (fromId = ?2 and toId = ?1)")
     List<Message> findAllBetween(Long user1Id, Long user2Id);
 }
